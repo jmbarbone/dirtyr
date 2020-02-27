5365d42041da462824060438b8bf4681d38f5557
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-02-27 12:17:36 -0500

    updates numerics for factor (and integer) handling

--- R/numerics.R
+++ R/numerics.R
@@ -6,21 +6,54 @@
 #' @param names Logical.  If true, supplies original value as name.
 #'
 #' @details
-#' The biggest difference in `as_numeric` compared to `as.numeric` is that it returns values of `NaN`
+#' The biggest difference in `as_numeric` compared to `as.numeric` is that it returns values of `NaN`.
 #' rather than `NA` and does not through an error.
+#' Factors are also specially handled as a numeric value is derived from the factor value rather
+#' than the factor level.
 #'
 #' `maybe_numeric` can be used to check is a value, possibly stored as text, can be safely converted.
 #'
 #' @export
 #'
 #' @examples
+#' ## with characters
 #' x <- c(as.character(1:5), NA, letters[1:2], "1e4")
 #' suppressWarnings(as.numeric(x))  ## will throw an error
 #' as_numeric(x)                    ## converts NA to NaN
 #' maybe_numeric(x, names = TRUE)
+#'
+#' ## With factors
+#' f <- factor(c(seq(0, 1, .2), "No", "Na_character_"))
+#' suppressWarnings(as.numeric(f))  ## uses factor level instead of text
+#' as_numeric(f)                    ## uses text values instead
+#' maybe_numeric(f, names = TRUE)
 
 as_numeric <- function(x) {
+  UseMethod("as_numeric", x)
+}
+
+#' @export
+as_numeric.default <- function(x) {
   n <- suppressWarnings(as.numeric(x))
+  n[is.na(n)] <- NaN
+  n
+}
+
+#' @export
+as_numeric.numeric <- function(x) {
+  x[is.na(x)] <- NaN
+  x
+}
+
+#' @export
+as_numeric.integer <- function(x) {
+  x[is.na(x)] <- NA_integer_
+  x
+}
+
+#' @export
+as_numeric.factor <- function(x) {
+  n <- suppressWarnings(as.numeric(levels(x))[x])
   n[is.na(n)] <- NaN
   n
 }
@@ -28,8 +61,23 @@ as_numeric <- function(x) {
 #' @export
 #' @rdname as_numeric
 maybe_numeric <- function(x, names = FALSE) {
+  UseMethod("maybe_numeric", x)
+}
+
+#' @export
+maybe_numeric.default <- function(x, names = FALSE) {
   vapply(x,
          function(x) !is.na(suppressWarnings(as.numeric(x))),
          logical(1),
          USE.NAMES = names)
 }
+
+#' @export
+maybe_numeric.factor <- function(x, names = FALSE) {
+  res <- vapply(x,
+                function(x) !is.na(suppressWarnings(as.numeric(levels(x))[x])),
+                logical(1))
+  if(names) names(res) <- as.character(x)
+  res
+}
+
---
+++ tests/testthat/test-numerics.R
@@ -0,0 +1,35 @@
+context("numerics")
+
+
+test_that("character example", {
+  x <- c(as.character(1:5), NA, letters[1:2], "1e4")
+
+  res <- as_numeric(x)
+  exp <- c(1, 2, 3, 4, 5, NaN, NaN, 10000)
+  expect_setequal(res, exp)
+
+  res <- maybe_numeric(x)
+  exp <- c(TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE)
+  expect_setequal(res, exp)
+
+  res <- maybe_numeric(x, names = TRUE)
+  expect_named(res)
+  expect_setequal(names(res), x)
+})
+
+
+test_that("factor example", {
+  f <- factor(c(seq(0, 1, .2), "No", "Na_character_"))
+
+  res <- as_numeric(f)
+  exp <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0, NaN, NaN)
+  expect_setequal(res, exp)
+
+  res <- maybe_numeric(f)
+  exp <- c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE)
+  expect_setequal(res, exp)
+
+  res <- maybe_numeric(f, names = TRUE)
+  expect_named(res)
+  expect_setequal(names(res), f)
+})
