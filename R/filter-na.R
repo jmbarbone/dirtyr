48910ccfd2eadad531c8c5e3d24881081321fb8d
jmbarbone <jmbarbone@gmail.com>
2020-02-28 08:48:22 -0500

    adds `keep_na()` and `keep_nin()`

--- R/filter-na.R
+++ R/filter-na.R
@@ -3,23 +3,48 @@
 #' Filter rows for missing data
 #'
 #' @param df A data.frame.
-#' @param cols Columns to include.  If NULL, all columns will be examined.
+#' @param ... Columns to include.  If not included, all columns filtered on.
 #'
+#' @details
+#' `...` supports tidy select
+#'
+#' @importFrom dplyr filter_at
+#' @importFrom dplyr vars
+#' @importFrom dplyr any_vars
 #' @export
 #' @name keep_na
+#' @examples
+#' \notrun{
+#' load(dirtyr)
+#' keep_na(dirty, V8)
+#' keep_nin(dirty, V4)
+#' }
 
-keep_na <- function(df, cols = NULL)
-{
-  cols <- if_then(df, is.null, colnames(df))
-  subset(df[cols], apply(df[cols], 1, anyNA))
+keep_na <- function(df, ...) {
+  v <- enquos(...)
+
+  if(length(v) == 0) {
+    filter_all(df, any_vars(anyNA(.)))
+  } else {
+    filter_at(df, vars(!!! v ), any_vars(anyNA(.)))
+  }
 }
+
+
 
 #' @export
 #' @rdname keep_na
-keep_nin <- function(df, cols) {
-  cols <- if_then(df, is.null, colnames(df))
-  subset(df[cols], apply(df[cols], 1, any_nin))
+keep_nin <- function(df, ...) {
+  v <- enquos(...)
+
+  if(length(v) == 0) {
+    filter_all(df, any_vars(any_nin(.)))
+  } else {
+    filter_at(df, vars(!!! v ), any_vars(any_nin(.)))
+  }
 }
+
+
 
 # df <- dplyr::sample_n(airquality, 10000, T)
 #
