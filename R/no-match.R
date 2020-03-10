023028d218d235fdc4f1b7566c4756ab214a6092
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-03-10 11:15:40 -0400

    replaces ``%w/o%` with `%wo%` and adds `%wi%`

--- R/no-match.R
+++ R/no-match.R
@@ -7,8 +7,9 @@
 #' @examples
 #' 1:10 %in% c(1,3,5,9)
 #' 1:10 %out% c(1,3,5,9)
+#' letters[1:5] %wo% letters[3:7]
+#' letters[1:5] %wi% letters[3:7]
 #'
-#' #' (1:10) %w/o% c(3,7,12)
 #' ## Note that setdiff() is very similar and typically makes more sense:
 #'         c(1:6,7:2) %w/o% c(3,7,12)  # -> keeps duplicates
 #' setdiff(c(1:6,7:2),      c(3,7,12)) # -> unique values
@@ -21,7 +22,22 @@
 #' @export
 
 `%w/o%` <- function(x, table) {
+  simpleMessage("Please use `%wo%` instead", call = FALSE)
+  x %wo% table
+}
+
+#' @rdname grapes-out-grapes
+#' @export
+
+`%wo%` <- function(x, table) {
   x[x %out% table]
+}
+
+#' @rdname grapes-out-grapes
+#' @export
+
+`%wi%` <- function(x, table) {
+  x[x %in% table]
 }
 
 #' @rdname grapes-out-grapes
