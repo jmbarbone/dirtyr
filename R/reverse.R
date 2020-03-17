7caf899a6f3d0380f008af51d8ee45d53dafbc1e
jmbarbone <jmbarbone@gmail.com>
2020-03-17 11:34:01 -0400

    adds reverse()

---
+++ R/reverse.R
@@ -0,0 +1,31 @@
+#' Reverse an object
+#'
+#' Reverses the order of an object or a data.frame
+#'
+#' @param x A vector, data.frame, or matrix
+#' @param ... Additional arguments sent to methods (not used)
+#' @export
+#'
+#' @examples
+#' reverse(letters)
+#' head(iris)
+#' reverse(iris) %>% tail()
+
+reverse <- function(x, ...) {
+  UseMethod("reverse", x)
+}
+
+#' @export
+reverse.default <- function(x, ...) {
+  rev(x)
+}
+
+#' @export
+reverse.data.frame <- function(x, ...) {
+ reverse.matrix(x)
+}
+
+#' @export
+reverse.matrix <- function(x, ...) {
+  if(nrow(x)) x[nrow(x):1, ] else x
+}
