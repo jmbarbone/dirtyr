f9f6fd57e9a2809b9af048d6dd793763387f26f7
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-03-22 10:10:41 -0400

    documents test data sets

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' Example dirty data
+#'
+#' A test data set
+#'
+#' @format A data frame with x rows and y variables
+#' \describe{
+#' \item{V1}{variable 1, description}
+#' \item{V2}{variable 2, description}
+#' \item{V3}{variable 3, description}
+#' \item{V4}{variable 4, description}
+#' \item{V5}{variable 5, description}
+#' }
+"dirty"
+
+#' Example dirty data (small)
+#'
+#' A test data set
+#'
+#' @format A data frame with x rows and y variables
+#' \describe{
+#' \item{V1}{variable 1, description}
+#' \item{V2}{variable 2, description}
+#' \item{V3}{variable 3, description}
+#' \item{V4}{variable 4, description}
+#' \item{V5}{variable 5, description}
+#' }
+"dirty_small"
+
+#' Example data with NAs
+#'
+#' A test data set
+#'
+#' @format A data frame with x rows and y variables
+#' \describe{
+#' \item{logical}{logical values}
+#' \item{character}{character values}
+#' \item{integer}{integer values}
+#' \item{double}{double values}
+#' \item{date}{date values}
+#' \item{infinity}{infinity values}
+#' }
+"na_data"
