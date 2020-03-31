e1889258ac7ecca1fbc79e36ddacddccaf63fd4e
jmbarbone <jmbarbone@gmail.com>
2020-03-31 12:53:05 -0400

    adds startup messages for .onLoad() and .onAttach()
    Yeah, I know it's not the best practice, but I'd rather have an annoying warning that this is development and to use at your own risk.

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Warning: This is a developmental package provided without any guarantee\n",
+    "of functionality or correctness.\n",
+    "Use at your own risk."
+    )
+}
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Reminder: Check 'github.com/jmbarbone/dirty'r for updates."
+    )
+}
