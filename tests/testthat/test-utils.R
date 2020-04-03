fcb6960b138c6fcfac1649bf5d719a2681ac572e
jmbarbone <jmbarbone@gmail.com>
2020-04-03 07:58:39 -0400

    shortens error message in `require_namespace()`

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,7 +2,7 @@ context("utils testing")
 
 test_that("require_namespace", {
   expect_error(require_namespace("not-real"),
-               'Package "not-real" needed for this function to work. Please install it.')
+               'Package "not-real" needed for this function to work')
 
   expect_null(require_namespace("testthat"))
 })
