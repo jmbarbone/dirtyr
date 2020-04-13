1e37180a945ad6b3936d7c65d243e9c2d868347f
jmbarbone <jmbarbone@gmail.com>
2020-04-12 23:40:20 -0400

    adds test for `are_different()`

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,12 @@ test_that("require_namespace", {
 
   expect_null(require_namespace("testthat"))
 })
+
+test_that("are different works", {
+  res <- are_different(
+    c(a = 1, b = 2, c = NA_character_, d = NA_character_),
+    c(a = 1, b = 3, c = NA_character_, d = 0)
+  )
+  exp <- c(a = FALSE, b = TRUE, c = FALSE, d = TRUE)
+  expect_equal(res, exp)
+})
