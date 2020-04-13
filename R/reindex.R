1706dc018ee8af020ce76ff947ea4baeae34a354
jmbarbone <jmbarbone@gmail.com>
2020-04-12 23:39:22 -0400

    updated reindex and adds test
    + Examples should probably be simplified
    + adds some simple tests to routinely check

--- R/reindex.R
+++ R/reindex.R
@@ -4,7 +4,7 @@
 #'
 #' @param x A data.frame or named vector
 #' @param ... Additional arguments passed to methods
-#' @param index The name of the column to reindex by
+#' @param index The name of the column to reindex by (see details)
 #' @param new_index The new order of the index as a vector
 #' @param keep_all Logical, whether to use only the index values in the
 #'   `new_index` or to use all available index values
@@ -13,31 +13,46 @@
 #' When implementing the `keep_all` argument, NA values in the same class as
 #'   the data.frame (df) are assigned.
 #' The `index` and `new_index` must be not have any duplicates.
+#' If a vector is passed, the `index` argument is not necessary.
+#' However, if this argument is set and `new_index` is missing, the latter will
+#'   take the value of the former.
 #'
 #' @importFrom stats setNames
 #' @export
 #'
 #' @examples
-#' iris1 <- head(iris, 5) %>% tibble::as_tibble()
+#' iris1 <- head(iris, 5)
 #' iris1$index <- 1:5
 #' reindex(iris1, "index", seq(2, 8, 2))
 #' reindex(iris1, "index", seq(2, 8, 2), keep_all = TRUE)
+#'
+#' x <- c("a" = 1, "b" = 2, "d" = 4)
+#' y <- setNames(nm = letters[2:5])
+#' reindex(x, new_index = y )
+#' reindex(x, new_index = y, keep_all = TRUE)
+#'
+#' x <- 2:5
+#' index <- NULL
+#' names(x) <- letters[x]
+#' new_index <- c(1, 3, 5, 7)
+#' names(new_index) <- letters[new_index]
+#' reindex(x, new_index = new_index)
+#' reindex(x, new_index = new_index, keep_all = TRUE)
+#'
+#' reindex(setNames(nm = letters[2:4]), letters[1:5])
 
-reindex <- function(x, index = NULL, new_index, keep_all = FALSE, ...) {
+reindex <- function(x, index, new_index, keep_all = FALSE, ...) {
   UseMethod("reindex", x)
 }
 
-# x <- 2:5
-# index <- NULL
-# names(x) <- letters[x]
-# new_index <- c(1, 3, 5, 7)
-# names(new_index) <- letters[new_index]
-# reindex(x, new_index = new_index, keep_all = FALSE)
-# reindex(x, new_index = new_index, keep_all = TRUE)
 
 #' @export
-reindex.default <- function(x, index = NULL, new_index, keep_all = FALSE, ...) {
-  stopifnot(is_named(x) & is.null(index))
+reindex.default <- function(x, index, new_index, keep_all = FALSE, ...) {
+  if(missing(new_index)) new_index <- index
+  stopifnot(is_named(x))
+  if(!is_named(new_index)) new_index <- setNames(nm = new_index)
+  stopifnot(unique_name_check(x) & unique_name_check(new_index))
+
   if(keep_all) {
     n <- sort(unique(c(names(x), names(new_index))))
     return(setNames(x[n], n))
@@ -46,12 +61,15 @@ reindex.default <- function(x, index = NULL, new_index, keep_all = FALSE, ...) {
 
 }
 
+# x <- a
+# index <- "index"
+# new_index <- b$index
+# keep_all = TRUE
+
 #' @export
-reindex.data.frame <- function(x, index = NULL, new_index, keep_all = FALSE, ...) {
-  stopifnot(!is.null(index))
+reindex.data.frame <- function(x, index , new_index, keep_all = FALSE, ...) {
   xi <- x[[index]]
-  stopifnot(identical(xi, unique(xi)))
-  stopifnot(identical(new_index, unique(new_index)))
+  stopifnot(unique_name_check(xi), unique_name_check(new_index))
   cn <- colnames(x)
   stopifnot(index %in% cn)
   m <- match(new_index, xi)
@@ -61,7 +79,7 @@ reindex.data.frame <- function(x, index = NULL, new_index, keep_all = FALSE, ...
     nas <- is.na(m)
     if(none(nas)) return(temp)
     ls <- lapply(x[cn[cn != index]], class_na)
-    ls[[index]] <- c(new_index[which(nas)])
+    ls[[index]] <- unique(c(xi %wo% new_index, c(new_index[which(nas)])))
     new_df <- as.data.frame(ls, stringsAsFactors = FALSE)
     cnt <- colnames(temp)
     temp <- r_bind(list(
@@ -71,4 +89,8 @@ reindex.data.frame <- function(x, index = NULL, new_index, keep_all = FALSE, ...
                     optional = TRUE)[cnt]))
   }
   as_tibble(temp)
+}
+
+unique_name_check <- function(x) {
+  all(unique(names(x)) == x)
 }
---
+++ tests/testthat/test-reindex.R
@@ -0,0 +1,36 @@
+context("reindex")
+
+test_that("index.default", {
+  a <- setNames(nm = letters[1:4])
+  b <- setNames(nm = letters[2:6])
+  res1 <- c(b = "b", c = "c", d = "d", e = NA_character_, f = NA_character_)
+  res2 <- append(c(a = "a"), res1)
+  expect_equal(reindex(a, b),
+               reindex(a, new_index = b))
+  expect_equal(reindex(a, b), res1)
+  expect_equal(reindex(a, b, keep_all = TRUE), res2)
+})
+
+test_that("index.data.frame", {
+  a <- data_frame(index = letters[1:4],
+                  var1 = 1:4,
+                  var2 = letters[1:4])
+  b <- data_frame(index = letters[2:6],
+                  var1 = 2:6,
+                  var2 = letters[1:5])
+
+  res1 <- a[which(!is.na(match(a$index, b$index))), ]
+
+  res2 <- list(data_frame(index = "a",
+                          var1 = NA_integer_,
+                          var2 = NA_character_),
+               res1,
+               data_frame(index = c("e", "f"),
+                          var1 = rep(NA_integer_, 2),
+                          var2 = rep(NA_character_, 2))) %>%
+    r_bind()
+
+
+  expect_equal(reindex(a, "index", b$index), res1)
+  expect_equal(reindex(a, "index", b$index, keep_all = TRUE), res2)
+})
